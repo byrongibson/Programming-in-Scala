6ef1d950ace45e2cf6e756cbee8d29eb7f2eb631
Byron Gibson <fbg111@gmail.com>
2011-07-09 18:56:53 -0700

    init:Checksummer.scala

---
+++ Checksummer.scala
@@ -0,0 +1,10 @@
+import ChecksumAccumulator.calculate
+
+object Checksummer {
+  def main(args: Array[String]) {
+    for (arg <- args)
+      println(arg + ": " + calculate(arg))
+  }
+}
+
+// vim: set ts=4 sw=4 et:
